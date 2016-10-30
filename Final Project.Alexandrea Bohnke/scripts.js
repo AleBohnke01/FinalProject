9bdb29de77f31b36ef60c15fa068aa6c3296ab31
Alexandrea Bohnke <alebohnke123@gmail.com>
2016-10-30 15:29:57 -0400

    I added my Stylesheet and Javascript to my main page.

---
+++ Final Project.Alexandrea Bohnke/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ale on 10/30/16.
+ */
+
+
+/** connect each verb/noun/etc to here,
+ * which will then connect to each other
+ * page where the connection needs to be...*/
+
+/** also need to make sure to allow for different choices to be selected...*/
+
+
